20ee930ced009985216d2ac7b6618ba18a0e87e7
Hazem Jday <hazemjday@gmail.com>
2022-07-16 18:18:41 +0000

    init projet with exceljs

---
+++ main.js
@@ -0,0 +1,15 @@
+const Excel = require('exceljs');
+
+var workbook = new Excel.Workbook();
+workbook.xlsx.readFile("g.xlsx")
+    .then(function () {
+        var worksheet = workbook.getWorksheet("Table 1");
+        worksheet.eachRow({ includeEmpty: true }, function (row, rowNumber) {
+            if (typeof e === typeof [0, 0])
+                for (let i = 0; i < length(row); i++) {
+                    console.log(row[i])
+                }
+            //console.log("Row " + rowNumber + " = " + JSON.stringify(row.values));
+
+        });
+    });

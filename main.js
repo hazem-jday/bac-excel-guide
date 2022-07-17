52b64afe04014d5bf7dc4e59e4da3b90537f3cb7
Hazem Jday <hazemjday@gmail.com>
2022-07-17 20:43:29 +0000

    adding more data

--- main.js
+++ main.js
@@ -1,57 +1,91 @@
 const Excel = require('exceljs');
 const fs = require('fs')
 
-const cho3ab = {by:"hazem-elyes"}
+const cho3ab = { by: "hazem-elyes" }
 var workbook = new Excel.Workbook();
 var code = 0
+var fac = ""
+var spec = ""
+var section = ""
+var changed = false
+var predCode =""
+var predSpec= ""
+var predFac=""
+var predSection=""
 workbook.xlsx.readFile("g.xlsx")
     .then(function () {
         var worksheet = workbook.getWorksheet("Table 1");
         worksheet.eachRow({ includeEmpty: false }, function (row, rowNumber) {
-            
 
             const rowValues = [];
             for (let value of row.values) {
                 if (typeof value === "object") {
-                    if (value !== null && typeof value.richText !== "undefined"){
+                    if (value !== null && typeof value.richText !== "undefined") {
                         rowValues.push(value.richText[0].text)
                     }
                 }
-                else if (typeof value === "number"){
+                else if (typeof value !== "undefined" && value !== null && value != "undefined") {
                     rowValues.push(value);
                 }
 
             }
 
-            values = rowValues.filter(function(item, pos, self) {
+            values = rowValues.filter(function (item, pos, self) {
                 return self.indexOf(item) == pos;
             })
-
+            console.log(values)
+            
             /*if(values.indexOf('ايرولاكبلا عون') >= 0){
                 console.log(values)
             }*/
 
-            if(values[0] === "-" || typeof values[0] === "number"){
-                if(typeof values[3] === "number"){
+
+
+            if (values[0] === "-" || typeof values[0] === "number") {
+                if (typeof values[3] === "number") {
                     code = values[3]
                 }
 
-                if(typeof values[3] !=="number"){
-                    if(!cho3ab[`${code}`])
-                        cho3ab[`${code}`]={}
-                    console.log(" . " + values[2]+" " + `${code}`)
-                    cho3ab[`${code}`][`${values[2]}`] = {score : values[0], formule : values[1]}
+                if(code === predCode){
+                    spec = ""
+                    fac = ""
                 }
-                else {
-                    if(!cho3ab[`${values[3]}`])
-                        cho3ab[`${values[3]}`]={}
-                    cho3ab[`${values[3]}`][`${values[2].split("").reverse().join("")}`]={score : values[0], formule : values[1]}
+                if(values.length === 7){
+                    if(predFac !== values[5]){
+                        fac += values[5]
+                        predFac = values[5]
+                    }
+                    if(predSpec !== values[4]){
+                        spec += values[4]
+                        predSpec = values[4]
+                    }
+                    if(predSection !== values[6]){
+                        section += values[6]
+                        predSection = values[6]
+                    }
                 }
 
-                console.log(values+"\n\n")
-                
+                cho3ab[`${code}`]["fac"] = fac
+
+
+                if (typeof values[3] !== "number") {
+                    if (!cho3ab[`${code}`])
+                        cho3ab[`${code}`] = {}
+                    //console.log(" . " + values[2]+" " + `${code}`)
+                    cho3ab[`${code}`][`${values[2]}`] = { score: values[0], formule: values[1] }
+                }
+                else {
+                    if (!cho3ab[`${code}`])
+                        cho3ab[`${code}`] = {}
+                    cho3ab[`${code}`][`${values[2].split("").reverse().join("")}`] = { score: values[0], formule: values[1] }
+                }
+
+                //console.log(values+"\n\n")
+
             }
-            
+
+
+
 
         });
         fs.writeFile('logs.txt', "----------\n", err => {
@@ -66,21 +100,9 @@ workbook.xlsx.readFile("g.xlsx")
                 return;
             }
         });
-        //console.log(cho3ab);
+        console.log(max);
 
     });
-function uniq(a) {
-    var prims = { "boolean": {}, "number": {}, "string": {} }, objs = [];
-
-    return a.filter(function (item) {
-        var type = typeof item;
-        if (type in prims)
-            return prims[type].hasOwnProperty(item) ? false : (prims[type][item] = true);
-        else
-            return objs.indexOf(item) >= 0 ? false : objs.push(item);
-    });
-}
-
 
 
 /*
